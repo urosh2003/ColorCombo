0d8dfe2e9d5ddf52ce328fc8e6bc3195987694fe
urosh2003 <radukic.uros@gmail.com>
2025-08-05 19:33:20 +0200

    Implemented enemy spawning

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,20 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private EnemyColor enemyColor;
     private SpriteRenderer spriteRenderer;
-       
+    private Rigidbody2D rigidbody;
+    private Vector2 movementDirection;
+    [SerializeField] private float movementSpeed;
+
+    private void Update()
+    {
+        movementDirection = PlayerManager.instance.transform.position - transform.position;
+        rigidbody.velocity = movementDirection.normalized * movementSpeed;
+    }
 
     private void Awake()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     public void SetEnemyColor(EnemyColor color)
