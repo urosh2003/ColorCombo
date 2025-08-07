bd4df146aad20804cce0792fd3b614d72af75c05
Uros Radukic <155995023+urosh2003@users.noreply.github.com>
2025-08-07 15:36:30 +0200

    Revert "Implemented combat"

--- Assets/Scripts/ProjectileColors/ProjectileColor.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ProjectileColorData", menuName = "ProjectileColorData")]
-public class ProjectileColor : ScriptableObject
-{
-    public Sprite sprite;
-    public WizardColor color;
-}
