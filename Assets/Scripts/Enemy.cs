bbfd532328981108dd81346294b640d1e0bb3da9
urosh2003 <radukic.uros@gmail.com>
2025-08-08 15:14:46 +0200

    Added enemy animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,11 +14,24 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float movementSpeedHigh;
     [SerializeField] private float deadSpriteDuration;
     private AudioSource audioSource;
+    private Animator animator;
+    [SerializeField] AnimatorOverrideController blueAnimator;
+    [SerializeField] AnimatorOverrideController redAnimator;
+    [SerializeField] AnimatorOverrideController yellowAnimator;
+    [SerializeField] AnimatorOverrideController greenAnimator;
+    [SerializeField] AnimatorOverrideController orangeAnimator;
+    [SerializeField] AnimatorOverrideController purpleAnimator;
+
 
     private void Update()
     {
         movementDirection = PlayerManager.instance.transform.position - transform.position;
         rigidbody.velocity = movementDirection.normalized * movementSpeed;
+
+        if (movementDirection.x > 0)
+            spriteRenderer.flipX = false;
+        else if (movementDirection.x < 0)
+            spriteRenderer.flipX = true;
     }
 
     private void Awake()
@@ -26,12 +39,33 @@ public class Enemy : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    public void SetEnemyColor(EnemyColor color, float currentSpawnRate)
+    public void SetEnemyColor(EnemyColor enemyColor, float currentSpawnRate)
     {
-        enemyColor = color;
-        spriteRenderer.sprite = enemyColor.sprite;
+        this.enemyColor = enemyColor;
+        switch (this.enemyColor.color)
+        {
+            case WizardColor.BLUE:
+                animator.runtimeAnimatorController = blueAnimator;
+                break;
+            case WizardColor.YELLOW:
+                animator.runtimeAnimatorController = yellowAnimator;
+                break;
+            case WizardColor.RED:
+                animator.runtimeAnimatorController = redAnimator;
+                break;
+            case WizardColor.GREEN:
+                animator.runtimeAnimatorController = greenAnimator;
+                break;
+            case WizardColor.ORANGE:
+                animator.runtimeAnimatorController = orangeAnimator;
+                break;
+            case WizardColor.PURPLE:
+                animator.runtimeAnimatorController = purpleAnimator;
+                break;
+        }
         movementSpeed = Random.Range(movementSpeedLow - currentSpawnRate, movementSpeedHigh - 2*currentSpawnRate);
     }
 
@@ -61,6 +95,7 @@ public class Enemy : MonoBehaviour
     {
         movementSpeed = 0;
         gameObject.GetComponent<CircleCollider2D>().enabled = false;
+        animator.StopPlayback();
         spriteRenderer.sprite = hitSprite;
         StartCoroutine(DisplayHitFrame());
     }
